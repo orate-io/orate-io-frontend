36346f93b6d79fe5e7c5c4f788fbefb79b2cf53a
Oussama Saoudi <oussama.sa216@gmail.com>
2021-06-19 15:47:39 -0400

    Added JSDoc compatibility with ESLint
    Added JSDoc linter and new rules for eslint. Fixed
    code to conform to new standards
    
    Signed-off-by: Oussama Saoudi <oussama.sa216@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+/**
+ * @file Defines Eslint rules that are which enforce the coding standard.
+ */
 module.exports = {
   env: {
     node: true,
@@ -13,40 +16,57 @@ module.exports = {
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
     sourceType: 'module'
   },
   plugins: [
-    'react', 'jest', 'cypress'
+    'react', 'jest', 'cypress', 'jsdoc'
   ],
   rules: {
-    indent: [
-      'warn',
-      2
-    ],
-    'linebreak-style': [
-      'warn',
-      'unix'
-    ],
-    quotes: [
-      'warn',
-      'single'
-    ],
-    semi: [
-      'warn',
-      'never'
-    ],
+    'jsdoc/check-access': 1,
+    'jsdoc/check-alignment': 1,
+    'jsdoc/check-tag-names': 1,
+    'jsdoc/check-types': 1,
+    'jsdoc/empty-tags': 1,
+    'jsdoc/implements-on-classes': 1,
+    'jsdoc/multiline-blocks': 1,
+    'jsdoc/newline-after-description': 1,
+    'jsdoc/no-multi-asterisks': 1,
+    'jsdoc/no-undefined-types': 1,
+    'jsdoc/require-jsdoc': 1,
+    'jsdoc/require-param': 1,
+    'jsdoc/require-param-description': 1,
+    'jsdoc/require-param-name': 1,
+    'jsdoc/require-param-type': 1,
+    'jsdoc/require-property': 1,
+    'jsdoc/require-property-description': 1,
+    'jsdoc/require-property-name': 1,
+    'jsdoc/require-property-type': 1,
+    'jsdoc/require-returns': 1,
+    'jsdoc/require-returns-check': 1,
+    'jsdoc/require-returns-description': 1,
+    'jsdoc/require-returns-type': 1,
+    'jsdoc/require-yields': 1,
+    'jsdoc/require-yields-check': 1,
+    'jsdoc/tag-lines': 1,
+    'jsdoc/valid-types': 1,
+    'jsdoc/require-file-overview': 1,
+    'jsdoc/require-description': 1,
+    'jsdoc/require-description-complete-sentence': 1,
+    'jsdoc/check-indentation': 1,
+    'indent': ['warn', 2],
+    'linebreak-style': ['warn', 'unix'],
+    'quotes': ['warn', 'single'],
+    'semi': ['warn', 'never'],
     'no-trailing-spaces': 'warn',
-    'object-curly-spacing': [
-      'warn',
-      'always'
-    ],
-    'arrow-spacing': [
-      'warn',
-      { before: true, after: true }
-    ],
-    'react/react-in-jsx-scope': 'off'
-  },
+    'object-curly-spacing': ['warn', 'always'],
+    'arrow-spacing': ['warn', { 'before': true, 'after': true }],
+    'react/react-in-jsx-scope': 'off',
+    'comma-spacing': ['warn', { 'before': false, 'after': true }],
+    'array-bracket-spacing': ['warn', 'never'],
+    'eol-last': 1,
+    'comma-dangle': ['warn', 'never']
+  }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+/**
+ * @file This is the test file for App.js.
+ */
 import { render } from '@testing-library/react'
 import App from './App'
 
