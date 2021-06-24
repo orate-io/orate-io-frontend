6cd5a2f8552a80d42a6adb550feb9a3430c2ec05
Yacine Saoudi <yac.sao453@gmail.com>
2021-06-24 16:17:49 -0400

    Database Integration -removed account reducer because it was pointless -moved signup request to the component itself -login and singup post requests both send the correct data to the backend
    Signed-off-by: Yacine Saoudi <yac.sao453@gmail.com>

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,28 +1,23 @@
-import userService from '../services/userService'
+// import userService from '../services/userService'
 
-export const accountReducer = (state = [], action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-  let newState = state
-  switch (action.type){
-  case 'SIGNUP':
-    newState = state.concat(action.signup)
-    console.log(newState)
-    return newState
-  default:
-    return state
-  }
-}
+// export const accountReducer = (state = [], action) => {
+//   let newState = state
+//   switch (action.type){
+//   case 'SIGNUP':
+//     newState = state.concat(action.signup)
+//     return state
+//   default:
+//     return state
+//   }
+// }
 
-export const signup = (newObj) => {
-  console.log('SIGNUP')
-  console.log(newObj)
-  return async dispatch => {
-    const signup = await userService.signupReq(newObj)
-    console.log(signup)
-    dispatch({
-      type: 'SIGNUP',
-      signup
-    })
-  }
-}
+// export const signup = (newObj) => {
+//   return async dispatch => {
+//     const signup = await userService.signupReq(newObj)
+//     console.log(signup)
+//     dispatch({
+//       type: 'SIGNUP',
+//       signup
+//     })
+//   }
+// }
