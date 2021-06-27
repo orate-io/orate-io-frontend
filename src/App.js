753de0b466185d2520e52602bf3d4a76fb0cdba4
Yacine Saoudi <yac.sao453@gmail.com>
2021-06-27 13:50:34 -0400

    Documentation update. documented the components and functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/**
+ * @file App is where all the components are called.
+ */
 import React from 'react'
 import Login from './components/Login.js'
 import Signup from './components/Signup.js'
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,49 @@
-import React from 'react'
+/**
+ * @file Login component, takes data from form when submitted and sends a dispatch to the login reducer, then sets local storage
+ * to the token.
+ * @author Yacine Saoudi
+ */
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from '../reducers/loginReducer'
+
 const Login = () => {
   const dispatch = useDispatch()
+  /**.
+   * @method handleSubmit - takes data from the forms, cretes an obejct with it, then sends it to the login reducer to make a request.
+   *
+   * @param event used to prevent the default action on button press; stops the page from refreshing too soonW
+   */
   const handleSubmit = async (event) => {
+
     event.preventDefault()
+
     const username = event.target.username.value
     const password = event.target.password.value
+
+    event.target.username.value = ''
+    event.target.password.value = ''
+
     const newObj = {
       username: username,
       password: password
     }
+
     dispatch(login(newObj))
   }
+  /**
+   * @function useEffect - on page start, check if the user is logged in using the token.
+   */
+  useEffect(() => {
+    const loggedIn = window.localStorage.getItem('loggedUser')
+    if (loggedIn){
+      console.log(loggedIn)
+    }
+  })
+
   return(
     <form onSubmit={handleSubmit}>
+
       <div>
           username:
         <input
@@ -22,6 +51,7 @@ const Login = () => {
           name = 'username'
         />
       </div>
+
       <div>
           password:
         <input
@@ -29,6 +59,7 @@ const Login = () => {
           name = 'password'
         />
       </div>
+
       <button type = "submit">submit</button>
     </form>
   )
