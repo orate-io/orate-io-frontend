929ee49bbe8a8d0c81a13193bb33f1e04991785f
Steven Tran <steventran0315@outlook.com>
2021-07-10 18:11:55 -0400

    fixed spacing and naming conventions
    Signed-off-by: Steven Tran <steventran0315@outlook.com>

--- src/App.js
+++ src/App.js
@@ -8,33 +8,33 @@ import './css/Login.css';
 
 const App = () => {
     const [loginVisible, setLoginVisible] = useState(false)
-    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+    const hideLoginWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showLoginWhenVisible = { display: loginVisible ? '' : 'none' }
 
     const [signinVisible, setSigninVisible] = useState(false)
-    const hideWhenVisible1 = { display: signinVisible ? 'none' : '' }
-    const showWhenVisible1 = { display: signinVisible ? '' : 'none' }
+    const hideSigninWhenVisible = { display: signinVisible ? 'none' : '' }
+    const showSigninWhenVisible = { display: signinVisible ? '' : 'none' }
 
 
     return (
         <h1>
             <p>Orate IO</p>
-            <div style={showWhenVisible}>
+            <div style={showLoginWhenVisible}>
                 <Login />
                 <button onClick={() => setLoginVisible(false)} id="secondaryButton">Cancel</button>
             </div>
-            <div style={hideWhenVisible}>
-                <div style={hideWhenVisible1}>
+            <div style={hideLoginWhenVisible}>
+                <div style={hideSigninWhenVisible}>
                     <button onClick={() => setLoginVisible(true)}>Log In</button>
                 </div>
             </div>
-            <div style={hideWhenVisible1}>
-                <div style={hideWhenVisible}>
+            <div style={hideSigninWhenVisible}>
+                <div style={hideLoginWhenVisible}>
                     <br></br>
                     <button onClick={() => setSigninVisible(true)} id="secondaryButton">Sign Up</button>
                 </div>
             </div>
-            <div style={showWhenVisible1}>
+            <div style={showSigninWhenVisible}>
                 <Signup />
                 <button onClick={() => setSigninVisible(false)} id="secondaryButton">Cancel</button>
             </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,84 +7,79 @@ import { useDispatch } from 'react-redux'
 import { login, logout } from '../reducers/loginReducer'
 import './../css/Login.css';
 
-
-
 const Login = () => {
 
+  const dispatch = useDispatch()
+  /**
+   * Takes data from the forms, cretes an obejct with it, then sends it to the login reducer to make a request.
+   *
+   * @param {object} event Prevents page from refreshing prematurely.
+   */
+  const handleSubmit = async (event) => {
 
-    const dispatch = useDispatch()
-    /**
-     * Takes data from the forms, cretes an obejct with it, then sends it to the login reducer to make a request.
-     *
-     * @param {object} event Prevents page from refreshing prematurely.
-     */
-    const handleSubmit = async (event) => {
+    event.preventDefault()
 
-        event.preventDefault()
+    const username = event.target.username.value
+    const password = event.target.password.value
 
-        const username = event.target.username.value
-        const password = event.target.password.value
+    event.target.username.value = ''
+    event.target.password.value = ''
 
-        event.target.username.value = ''
-        event.target.password.value = ''
-
-        const newObj = {
-            username: username,
-            password: password
-        }
-
-        dispatch(login(newObj))
-    }
-    /**
-     * Sends a logout dispatch to the action creator.
-     */
-    const logoutHandler = async () => {
-        dispatch(logout('empty'))
+    const newObj = {
+      username: username,
+      password: password
     }
 
+    dispatch(login(newObj))
+  }
+  /**
+   * Sends a logout dispatch to the action creator.
+   */
+  const logoutHandler = async () => {
+    dispatch(logout('empty'))
+  }
 
-    /* renders a logout button if logged in, login form if otherwise */
-    let loggedIn = window.localStorage.getItem('loggedUser')
-    if (loggedIn) {
-        return (
-            <div>
-                <button type="submit" onClick={logoutHandler} > Logout </button>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div>
-                <form onSubmit={handleSubmit}>
+  /* renders a logout button if logged in, login form if otherwise */
+  let loggedIn = window.localStorage.getItem('loggedUser')
+  if (loggedIn) {
+    return (
+      <div>
+        <button type="submit" onClick={logoutHandler} > Logout </button>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
 
-                    <div>
+          <div>
+            <input
+              type='text'
+              placeholder='Username'
+              id="textbox"
+            />
+          </div>
 
-                        <input
-                            type='text'
-                            placeholder='Username'
-                            id="textbox"
-                        />
-                    </div>
+          <div>
+            <input
+              type='password'
+              placeholder='Password'
+              id="textbox"
+            />
+          </div>
 
-                    <div>
-                        <input
-                            type='password'
-                            placeholder='Password'
-                            id="textbox"
-                        />
-                    </div>
+          <br></br>
+          <button id="submit" type="submit">Log In</button>
+          <div>
+            <br></br>
+          </div>
+        </form>
 
-                    <br></br>
-                    <button id="submit" type="submit">Log In</button>
-                    <div>
-                        <br></br>
-                    </div>
-                </form>
+      </div>
 
-            </div>
-
-        )
-    }
+    )
+  }
 
 }
 
