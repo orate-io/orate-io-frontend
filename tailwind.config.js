8939be365dc49afd532f3f554576c4c7baf75ef3
Oussama Saoudi <oussama.sa216@gmail.com>
2021-08-17 19:02:32 -0400

    init tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/**
+ * @file Configuration file for Tailwind CSS.
+ */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
